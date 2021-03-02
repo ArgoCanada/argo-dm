5d737bb3c838cc5f8134e0c1c84956f3f8ea4532
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-03-02 08:57:44 -0400

    add dataplans, change color palette

--- src/data_plan.py
+++ src/data_plan.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(style='whitegrid')
+pal = sns.color_palette('colorblind')
 
 df = pd.read_csv(Path('../data/nke-npts-npacket.csv'))
 df['nsbd'] = np.ceil(df.npacket/3)
@@ -28,13 +29,19 @@ meds_bgc_data = np.array([3090,5200,3221,4967,5167])/1000
 fig, ax = plt.subplots()
 ax.plot(df.npts, df.data_kb, 'ko', label='NKE Core')
 ax.plot(df.npts, df.data_bgc_kb, '^', color='grey', label='NKE BGC')
-ax.plot(meds_core_pts, meds_core_data, 'o', color='orange', label='MEDS Core')
-ax.plot(coriolis_core_pts, coriolis_core_data, 'mo', label='Coriolis Core')
-ax.plot(meds_bgc_pts, meds_bgc_data, '^', color='orange', label='MEDS BGC')
+ax.plot(meds_core_pts, meds_core_data, 'o', color=pal[0], label='MEDS Core')
+ax.plot(coriolis_core_pts, coriolis_core_data, 'o', color=pal[1], label='Coriolis Core')
+ax.plot(meds_bgc_pts, meds_bgc_data, '^', color=pal[0], label='MEDS BGC')
+
+dplans = [12, 17, 30]
+pnames = ['B', 'F', 'G']
+for i,d in enumerate(dplans):
+    ax.axhline(d/3, color=pal[2+i], label=None)
+    ax.text(800, d/3+0.1, 'Plan {}/3'.format(pnames[i]))
 
 ax.set_xlabel('Average Number of Points per Profile')
-ax.set_ylabel('File Size per Profile (kB)')
+ax.set_ylabel('SBD File Size per Profile (kB)')
 ax.legend(loc=2)
 ax.set_xlim((-5, 1050))
 ax.set_ylim((0,20))
-plt.show()
+fig.savefig(Path('../figures/approximate_data_usage.png'), bbox_inches='tight', dpi=350)
