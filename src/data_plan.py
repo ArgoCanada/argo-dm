b8f78ba80a9f2bfdf3c2f8e9fdb29b1be7dde499
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-04-26 09:18:31 -0300

    updated with RBR estimates

--- src/data_plan.py
+++ src/data_plan.py
@@ -14,8 +14,10 @@ pal = sns.color_palette('colorblind')
 df = pd.read_csv(Path('../data/nke-npts-npacket.csv'))
 df['nsbd'] = np.ceil(df.npacket/3)
 df['nsbd_bgc'] = np.ceil(df.npacket_bgc/3)
+df['nsbd_rbr'] = np.ceil(df.npacket_rbr/3)
 df['data_kb'] = df.nsbd*300/1000
 df['data_bgc_kb'] = df.nsbd_bgc*300/1000
+df['data_rbr_kb'] = df.nsbd_rbr*300/1000
 
 meds_core_pts  = np.array([129, 105, 104, 105, 104])
 meds_core_data = np.array([6400, 1800, 1757, 1743, 1785])/1000
@@ -29,6 +31,7 @@ meds_bgc_data = np.array([3090,5200,3221,4967,5167])/1000
 fig, ax = plt.subplots()
 ax.plot(df.npts, df.data_kb, 'ko', label='NKE Core')
 ax.plot(df.npts, df.data_bgc_kb, '^', color='grey', label='NKE BGC')
+ax.plot(df.npts, df.data_rbr_kb, 's', color='navy', label='NKE RBR')
 ax.plot(meds_core_pts, meds_core_data, 'o', color=pal[0], label='MEDS Core')
 ax.plot(coriolis_core_pts, coriolis_core_data, 'o', color=pal[1], label='Coriolis Core')
 ax.plot(meds_bgc_pts, meds_bgc_data, '^', color=pal[0], label='MEDS BGC')
@@ -36,14 +39,16 @@ ax.plot(meds_bgc_pts, meds_bgc_data, '^', color=pal[0], label='MEDS BGC')
 
 cf = np.poly1d(np.polyfit(df.data_kb, df.npts, 1))
 bf = np.poly1d(np.polyfit(df.data_bgc_kb, df.npts, 1))
+rf = np.poly1d(np.polyfit(df.data_rbr_kb, df.npts, 1))
 
 dplans = [12, 17, 30]
 pnames = ['2', '3', '4']
 for i,d in enumerate(dplans):
     ax.axhline(d/3, color=pal[2+i], label=None)
-    ax.axvline(cf(d/3), color=pal[2+i], linestyle='--', label=None)
-    ax.axvline(bf(d/3), color=pal[2+i], linestyle='-.', label=None)
-    print(cf(d/3), bf(d/3))
+    ax.axvline(cf(d/3), color=pal[2+i], linestyle='-', label=None)
+    ax.axvline(bf(d/3), color=pal[2+i], linestyle='--', label=None)
+    ax.axvline(rf(d/3), color=pal[2+i], linestyle='-.', label=None)
+    print(cf(d/3), bf(d/3), rf(d/3))
     ax.text(800, d/3+0.1, 'Plan {}/3'.format(pnames[i]), color=pal[2+i])
 
 ax.plot([448], [3.9], '*', color='k', markersize=10, label='New High-res MEDS Float')
@@ -55,5 +60,5 @@ ax.set_xlim((-5, 1050))
 ax.set_ylim((0,20))
 ax.minorticks_on()
 ax.grid()
-fig.savefig(Path('../figures/approximate_data_usage_20210325.png'), bbox_inches='tight', dpi=350)
+fig.savefig(Path('../figures/approximate_data_usage_20210422.png'), bbox_inches='tight', dpi=350)
 plt.close(fig)
