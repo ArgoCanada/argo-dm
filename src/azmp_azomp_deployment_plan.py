8b05822808932368c115fc734fb1a92402ac1143
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-02-16 15:12:06 -0400

    move east to include nfld deployments

--- src/azmp_azomp_deployment_plan.py
+++ src/azmp_azomp_deployment_plan.py
@@ -16,7 +16,7 @@ import cartopy.feature as cfeature
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
 # define rectangle around atlantic canada
-atl = [-68, -48, 40, 62]
+atl = [-68, -42, 40, 62]
 # choose projection/transform
 projection = ccrs.LambertConformal(central_latitude=50, central_longitude=-57.5)
 transform = ccrs.PlateCarree()
@@ -26,10 +26,10 @@ azmp = pd.read_csv(Path('../data/azmpStationsCore.txt'))
 ar7w = pd.read_csv(Path('../data/AZOMP_AR7W_stn.csv'))
 
 # create figure, add coastlines
-fig = plt.figure()
+fig = plt.figure(facecolor='beige')
 ax = fig.add_subplot(projection=projection)
 ax.set_extent(atl)
-ax.add_feature(cfeature.GSHHSFeature('intermediate', edgecolor='black', facecolor='lightgray'))
+ax.add_feature(cfeature.GSHHSFeature('intermediate', edgecolor='black', facecolor='beige'))
 
 # get bathymetry
 bath_file = Path('/Users/GordonC/Documents/data/GEBCO/GEBCO_2020.nc')
@@ -54,19 +54,21 @@ im = ax.contourf(
     cmap=cmo.deep,
     vmin=0, extend='max'
 )
-plt.colorbar(im, ax=ax, orientation='vertical', label='Depth (m)')
+cb = plt.colorbar(im, ax=ax, orientation='vertical', label='Depth (m)')
+cb.ax.set_facecolor(fig.get_facecolor())
 
 # plot stations
 ms = 2
 ax.plot(azmp.longitude, azmp.latitude, 'o', transform=transform, markersize=ms, label=None)
 ax.plot(ar7w.lon_dd, ar7w.lat_dd, 'o', transform=transform, markersize=ms, label=None)
 
-rbr_lats = [41.414172, 57.800000, 58.216667]
-rbr_lons = [-60.664633, -51.340000, -50.883333]
-core_lats = rbr_lats + [43.783300, 41.776870,  56.956667, 59.066667]
-core_lons = rbr_lons + [-57.833300, -60.906963,  -52.236667, -49.950000]
-bgc_lats = [42.475000, 42.026080, 43.473300, 57.376667, 58.640000, 56.536667, 59.483333]
-bgc_lons = [-61.433300, -61.068143, -57.526700, -51.791667, -50.416667, -52.678333, -49.475000]
+deploy = pd.read_csv(Path('../data/deployment-plan-spring-2022.csv'))
+rbr_lats = deploy.Latitude[deploy.Model == 'ARVOR-I-RBR']
+rbr_lons = deploy.Longitude[deploy.Model == 'ARVOR-I-RBR']
+core_lats = deploy.Latitude[deploy.Model == 'ARVOR-I']
+core_lons = deploy.Longitude[deploy.Model == 'ARVOR-I']
+bgc_lats = deploy.Latitude[deploy.Model == 'ARVOR-I+O2']
+bgc_lons = deploy.Longitude[deploy.Model == 'ARVOR-I+O2']
 
 ms = 6
 ax.plot(core_lons, core_lats, '*', label='CTD', markersize=ms, transform=transform)
@@ -82,13 +84,16 @@ gl.right_labels = False
 gl.xformatter = lon_formatter
 gl.yformatter = lat_formatter
 
-# ns = [-68, -56, 40, 51]
-# ax.set_extent(ns)
+ns = [-68, -56, 40, 51]
+ax.set_extent(ns)
 
 # lab_sea = [-60, -45, 52, 61]
 # ax.set_extent(lab_sea)
 
 # save fig
-fig.savefig(Path('../figures/azmp_azomp_deployments_2022.png'), dpi=350, bbox_inches='tight')
-
+fig.savefig(
+    Path('../figures/azmp_azomp_deployments_2022_ns.png'), 
+    dpi=350, bbox_inches='tight', facecolor=fig.get_facecolor(),
+    edgecolor=None
+)
 plt.show()
