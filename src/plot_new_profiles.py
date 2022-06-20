0247636a91e4d2257a14d2672ac0b4f516c11c55
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-06-20 14:38:01 -0300

    color flags more appropraitely

--- src/plot_new_profiles.py
+++ src/plot_new_profiles.py
@@ -3,6 +3,7 @@
 import sys
 
 # data modules
+import numpy as np
 import pandas as pd
 import argopandas as argo
 
@@ -35,27 +36,40 @@ bx = bx[bx.institution == 'ME']
 
 # get data, note bgc data will have to be updated/not
 # hard coded once the PROVOR floats are deployed
+core_vars = ['PRES', 'TEMP', 'TEMP_QC', 'PSAL', 'PSAL_QC']
+bgc_vars  = ['PRES', 'DOXY', 'DOXY_QC']
 if not ix.empty:
-    core = ix.levels[['PRES', 'TEMP', 'TEMP_QC', 'PSAL', 'PSAL_QC']]
+    core = ix.levels[core_vars]
 if not bx.empty:
-    bgc  = bx.levels[['PRES', 'DOXY', 'DOXY_QC']]
+    bgc  = bx.levels[bgc_vars]
 
+flags = [b'1', b'2', b'3', b'4', b'5', b'8']
+qc_dict = {v:6*[np.nan] for v in core_vars}
+qc_dict['TEMP_QC'] = flags
+qc_dict['PSAL_QC'] = flags
+qc = pd.DataFrame(qc_dict)
+palette = ['#229954', '#2874A6', '#E67E22', '#E74C3C', '#6C3483', '#2E4053']
 # plot the core data
 for f, pt in zip(ix.file, ix.profiler_type):
     fig, axes = plt.subplots(1, 3)
-    sns.scatterplot(x='TEMP', y='PRES', hue='TEMP_QC', style='N_PROF', data=core.loc[f], ax=axes[0], linewidth=0.1)
-    sns.scatterplot(x='PSAL', y='PRES', hue='PSAL_QC', style='N_PROF', data=core.loc[f], ax=axes[1], linewidth=0.1)
-    sns.scatterplot(x='PSAL', y='TEMP', data=core.loc[f], ax=axes[2], linewidth=0.1)
-    axes[0].set_title(f'{f}, Npts = {core.loc[f].loc[core.loc[f].PRES.notna()].shape[0]}, float type: {pt}', loc='left')
+    d = core.loc[f]
+    d = pd.concat([qc, d])
+    sns.scatterplot(x='TEMP', y='PRES', hue='TEMP_QC', data=d, ax=axes[0], linewidth=0.1, palette=palette)
+    sns.scatterplot(x='PSAL', y='PRES', hue='PSAL_QC', data=d, ax=axes[1], linewidth=0.1, palette=palette, legend=False)
+    sns.scatterplot(x='PSAL', y='TEMP', data=d, ax=axes[2], linewidth=0.1)
+    axes[0].set_title(f'{f}, Npts = {d.loc[d.PRES.notna()].shape[0]}, float type: {pt}', loc='left')
     [ax.set_ylim((2050, -50)) for ax in axes[:2]]
     fig.set_size_inches(2*fig.get_figwidth(), fig.get_figheight())
     fig.tight_layout()
     plt.show()
 
 # plot the bgc data
+qc_dict = {v:6*[np.nan] for v in bgc_vars}
+qc_dict['DOXY_QC'] = flags
+qc = pd.DataFrame(qc_dict)
 for f, pt in zip(bx.file, bx.profiler_type):
     fig, ax = plt.subplots()
-    sns.scatterplot(x='DOXY', y='PRES', hue='DOXY_QC', style='N_PROF', data=bgc.loc[f], ax=ax, linewidth=0.1)
+    sns.scatterplot(x='DOXY', y='PRES', hue='DOXY_QC', data=bgc.loc[f], ax=ax, linewidth=0.1)
     ax.set_ylim((2050, -50))
     ax.set_title(f'{f}, Npts = {bgc.loc[f].loc[bgc.loc[f].DOXY.notna()].shape[0]}, float type: {pt}', loc='left')
     plt.show()
