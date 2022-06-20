2f472bc626d1b8019902219a0adab36e73d63a66
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-06-20 14:38:13 -0300

    code to upload command file to rudics server

---
+++ src/upload_rudics_command.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from pathlib import Path
+import ftplib
+
+site = 'ftp.joubeh.com'
+ftp  = ftplib.FTP(site, user='dfobio', passwd='976143')
+
+imei_numbers = [
+    '300125061656740',
+    '300125061360970',
+]
+
+upload_file = Path('/Users/GordonC/Documents/argo/meds/predeployment-testing/RUDICS_CMD.txt')
+
+for imei in imei_numbers:
+    ftp.cwd(imei)
+    file = open(upload_file,'rb')
+    ftp.storbinary('STOR remote/RUDICS_cmd.txt', file)
+    file.close()
+    ftp.cwd('../')
+ftp.quit()
