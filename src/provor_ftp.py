18b235ea87dd68b03d54f1dcd8a18e875b167c89
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2023-11-30 09:44:49 -0400

    cts5 programming

--- src/provor_ftp.py
+++ src/provor_ftp.py
@@ -34,3 +34,17 @@ for imei in ftp.nlst():
             lf.close()
             sys.stdout.write('done\n')
     ftp.cwd('../')
+
+for imei in ftp.nlst():
+    ftp.cwd(imei)
+    for fn in ftp.nlst('*.hex'):
+        local_file = Path('../data/provor') / imei / fn
+        if not local_file.parent.exists():
+            local_file.parent.mkdir()
+        if not local_file.exists():
+            sys.stdout.write(f'Downloading {imei}/{fn}...')
+            lf = open(local_file, 'wb')
+            ftp.retrbinary('RETR ' + fn, lf.write)
+            lf.close()
+            sys.stdout.write('done\n')
+    ftp.cwd('../')
