33ea98346204f1d6dbd2e3fab408c8624680e022
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2024-02-01 08:19:15 -0400

    coords from index

--- src/read_plot_in-air_doxy.py
+++ src/read_plot_in-air_doxy.py
@@ -8,8 +8,12 @@ import matplotlib.pyplot as plt
 ### PUT PATH TO YOUR FILE HERE ###
 traj_file = Path('/Users/GordonC/Documents/data/Argo/dac/meds/4902481/4902481_BRtraj.nc')
 
+### PUT PATH TO YOUR INDEX FILE HERE - WILL NEED TO BE UP TO DATE ###
+index_file = Path('/Users/GordonC/Documents/projects/bgcArgoDMQC/bgcArgoDMQC/resource/data/Index/ar_index_global_prof.txt.gz')
+
 # load netCDF dataset
 traj = Dataset(traj_file)
+index = pd.read_csv(index_file, compression='gzip', header=8)
 
 # index for in-air data
 ix_in_air = traj['PRES'][:].data <= 0
@@ -18,6 +22,19 @@ ix_in_air = traj['PRES'][:].data <= 0
 in_air_doxy = traj['DOXY'][:].data[ix_in_air]
 juld = traj['JULD'][:].data[ix_in_air]
 cycle = traj['CYCLE_NUMBER'][:].data[ix_in_air]
+
+# reduce index to only include our float
+index['wmo'] = [int(f.split('/')[1]) for f in index.file]
+index = index.loc[index.wmo == 4902481]
+# get cycle number and profile direction
+index['cycle'] = [int(f.split('_')[1].strip('.nc').strip('D')) for f in index.file]
+# profile direction - we don't want descending profiles
+index['direction'] = [f.split('.')[1][-1] if f.split('.')[1][-1] == 'D' else 'A' for f in index.file]
+index = index.loc[index.direction == 'A']
+
+# resolve cycle/lat/long from index
+lat = pd.Series([index.loc[index.cycle == c].latitude.iloc[0] for c in cycle])
+long = pd.Series([index.loc[index.cycle == c].longitude.iloc[0] for c in cycle])
 
 # convert JULD (days since 1950) to pandas Timestamp
 # datetime unix epoch is since 1970, Argo is from 1950, so need to adjust
